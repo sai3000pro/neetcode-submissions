cf338c2a553d3fcaa8c297a511b77922e5c789d6
Sai Jilla <96095408+sai3000pro@users.noreply.github.com>
2026-08-31 14:21:43 -0400

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS = len(board)
+        COLS = len(board[0])
+        n = len(word)
+        def dfs(visited: List[Tuple[int]], row, col, index) -> bool:
+            if index == n:
+                return True
+            elif row < 0 or row >= ROWS:
+                return False
+            elif col < 0 or col >= COLS:
+                return False
+            else:
+                res = False
+                if (row, col) in visited:
+                    return False
+                if board[row][col] == word[index]:
+                    visited.add((row, col))
+                    res = dfs(set(visited), row + 1, col, index + 1) or dfs(set(visited), row, col + 1, index + 1) or dfs(set(visited), row - 1, col, index + 1) or dfs(set(visited), row, col - 1, index + 1)
+                return res
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                visited = set()
+                if dfs(visited, row, col, 0):
+                    return True
+        return False
